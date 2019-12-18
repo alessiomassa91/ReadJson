d820fae318696a2ab8abe23c7cfe9653da569454
alessiomassa91 <alessiomassa.engr@gmail.com>
2019-12-18 11:29:42 +0100

    Initial Commit

---
+++ ReadJson/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReadJson
+//
+//  Created by Alessio Massa on 18/12/2019.
+//  Copyright © 2019 Alessio Massa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
