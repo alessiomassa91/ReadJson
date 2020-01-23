bafec3e6f8a7f462d4702178ec22c1b6e7c9bbe4
alessiomassa91 <alessiomassa.engr@gmail.com>
2020-01-23 17:46:39 +0100

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '13.0'
+
+target 'ReadJson' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ReadJson
+  pod 'Alamofire'
+  pod 'AlamofireImage'
+  pod 'SwiftyJSON'
+  
+  target 'ReadJsonTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ ReadJson/Model/Home/HomeModel.swift
@@ -0,0 +1,22 @@
+//
+//  HomeModel.swift
+//  ReadJson
+//
+//  Created by Alessio Massa on 23/12/2019.
+//  Copyright © 2019 Alessio Massa. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+struct HomeModel {
+    var name: String = ""
+    var image_app: String = ""
+    var description: String = ""
+    var price: String = ""
+    var link_app: String = ""
+    var artist_name: String = ""
+    var category_name: String = ""
+    var release_data: String = ""
+    var position: Int
+}
